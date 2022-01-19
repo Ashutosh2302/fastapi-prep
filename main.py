90c7041c80d33ef919ff2a3b4a63edb7c7f1015a
Ashutosh <ashutoshb2000@gmail.com>
2022-01-19 15:34:50 +0530

    sqllite backend connection

--- main.py
+++ main.py
@@ -1,35 +1,55 @@
-from fastapi import FastAPI
-from fastapi.staticfiles import StaticFiles
-from fastapi.templating import Jinja2Templates
-import schema
-from database import SessionLocal, engine
-import model
-from fastapi import FastAPI, Depends, Request
+from typing import List
+
+from fastapi import Depends, FastAPI, HTTPException
 from sqlalchemy.orm import Session
-from fastapi.responses import HTMLResponse
+
+import crud, models, schemas
+from database import SessionLocal, engine
+
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
-model.Base.metadata.create_all(bind=engine)
-@app.get("/")
-def read_root():
-    return {"message": "welcome to FastAPI!"}
 
-def get_database_session():
+
+# Dependency
+def get_db():
+    db = SessionLocal()
     try:
-        db = SessionLocal()
         yield db
     finally:
         db.close()
 
-async def read_movies(request: Request, db: Session = Depends(get_database_session)):
-    records = db.query(Movie).all()
-    return templates.TemplateResponse("index.html", {"request": request, "data": records})
 
-@app.get("/movie/{name}", response_class=HTMLResponse)
-def read_movie(request: Request, name: schema.Movie.name, db: Session = Depends(get_database_session)):
-    item = db.query(Movie).filter(Movie.id==name).first()
-    print(item)
-    return templates.TemplateResponse("overview.html", {"request": request, "movie": item})
+@app.post("/users/", response_model=schemas.User)
+def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    db_user = crud.get_user_by_email(db, email=user.email)
+    if db_user:
+        raise HTTPException(status_code=400, detail="Email already registered")
+    return crud.create_user(db=db, user=user)
 
-app.mount("/static", StaticFiles(directory="static"), name="static")
-templates = Jinja2Templates(directory="templates")
+
+@app.get("/users/", response_model=List[schemas.User])
+def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    users = crud.get_users(db, skip=skip, limit=limit)
+    return users
+
+
+@app.get("/users/{user_id}", response_model=schemas.User)
+def read_user(user_id: int, db: Session = Depends(get_db)):
+    db_user = crud.get_user(db, user_id=user_id)
+    if db_user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    return db_user
+
+
+@app.post("/users/{user_id}/items/", response_model=schemas.Item)
+def create_item_for_user(
+    user_id: int, item: schemas.ItemCreate, db: Session = Depends(get_db)
+):
+    return crud.create_user_item(db=db, item=item, user_id=user_id)
+
+
+@app.get("/items/", response_model=List[schemas.Item])
+def read_items(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    items = crud.get_items(db, skip=skip, limit=limit)
+    return items
